32c176d74349ed78f5d4fbfa5398380399fd77d9
Surya T <apple@apple-macbook-pro-3.local>
2022-05-17 20:31:40 +0530

    update readme

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export const API_BASE_URL = "https://api.themoviedb.org/3";
 export const API_IMAGE_BASE_URL = "https://image.tmdb.org/t/p";
-export const API_KEY = "fdaf49ce88dfd2b301002c729df692d7";
+// Get API key from https://developers.themoviedb.org/3/getting-started/introduction
+export const API_KEY = "";
